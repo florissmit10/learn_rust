6b55e29a73c5bac04c6ac46c029699931b38b89f
Floris Smit <floris@xomnia.com>
2023-06-17 10:32:29 +0200

    Add shuttle-launchpad issue 1

---
+++ shuttle-launchpad/web-project/src/main.rs
@@ -0,0 +1,20 @@
+use std::net::SocketAddr;
+use axum::{response::IntoResponse, routing::get, Router};
+
+
+async fn hello_world() -> impl IntoResponse {
+    "Hello world"
+}
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(hello_world));
+
+    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
+
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
